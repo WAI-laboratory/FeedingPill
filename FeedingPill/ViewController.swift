20417e8e3dde662a65ee5bd4a744db1f96a0f8a5
Yongjun Lee <stareta1202@gmail.com>
2022-10-17 13:08:13 +0900

    Initial Commit

---
+++ FeedingPill/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FeedingPill
+//
+//  Created by layton on 2022/10/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
